4754672a77411f7b09983a2e6ccac1436c14be41
Наталья <123063048+Nataliy1997@users.noreply.github.com>
2023-02-12 11:27:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+//число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 ->
+
+void main(string[]args)
+{
+    Console.WriteLine("Введи число: ");
+    int n=int.Parse(Console.ReadLine()!);
+    int sum=0;
+    int ost=0;
+    {
+        while (n>0)
+        {
+            ost=n%10;
+            sum=sum+ost;
+            n=n/10;
+        }
+        Console.WriteLine($"Сума цифр числа равна {sum}");
+    }
+}
+main(args);
+
